ad1939d2c72ea99ed5905a96bbada1b30710f515
GABRIEL VIEIRA DE SOUSA <gabriel_v_de-sousa@estudante.sesisenai.org.br>
2026-08-19 15:38:24 -0300

    Versõa 1

---
+++ front_end/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import "../styles/Home.css"
+
+function Home() {
+  return (
+    <div class="container">
+      <div class="Header"> </div>
+      <div class="insider"> </div>
+      <div class="Home"> f</div>
+    </div>
+  )
+}
+
+export default Home()
